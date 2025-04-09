d4fce7e5538c2389c9ba6ec6a2c386323379bbe2
keasy9 <keasy9@gmail.com>
2025-04-09 21:28:00 +0700

    случайный цвет стрельбы

--- src/objects/Bullet.ts
+++ src/objects/Bullet.ts
@@ -4,6 +4,7 @@ const P_BULLET = 'bullet-white.png'
 
 export class Bullet extends Phaser.GameObjects.Sprite {
     protected static speed = 400;
+    protected static color: number;
 
     constructor(scene: Scene, x: number, y: number) {
         super(scene, x, y, P_BULLET);
@@ -17,6 +18,17 @@ export class Bullet extends Phaser.GameObjects.Sprite {
             P_SPRITES + P_BULLET,
             { frameWidth: 8, frameHeight: 8 },
         );
+
+        const colors = [
+            [0, 200, 255],
+            [255, 200, 255],
+            [255, 0, 100],
+            [0, 255, 100],
+            [255, 255, 0],
+        ];
+
+        // @ts-ignore
+        Bullet.color = Phaser.Display.Color.GetColor(...Phaser.Math.RND.pick(colors));
     }
 
     create() {
@@ -41,7 +53,7 @@ export class Bullet extends Phaser.GameObjects.Sprite {
         body.setSize(this.width, this.height)
             .setOffset(2, 0);
 
-        this.setTint(Phaser.Display.Color.GetColor(0, 200, 255));
+        this.setTint(Bullet.color);
     }
 
     onWorldBounds() {
